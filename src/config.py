22a08986b4e9ad0a8dbbdba4a129c16f4111b2af
Gopalakrishnan R <rgkrishnas@gmail.com>
2020-11-21 23:58:05 +0530

    Create config.py

---
+++ src/config.py
@@ -0,0 +1,10 @@
+#### This is developed by Gopal rgkrishnas@gmail.com for the main program mqtt_publish.py
+
+# Define the delay  time
+delay_time = 5
+# MQTT Host name or IP address with tcp
+broker_address= 'localhost'
+port = 1883  # MQTT port number
+user = ''  # MQTT user id if available
+password = '' # MQTT password if available
+topic = 'iot2tangle'  # MQTT message topic 
