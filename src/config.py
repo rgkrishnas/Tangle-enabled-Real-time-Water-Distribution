368143866e72052193429e7b24cfc5a16a3a1c41
Gopalakrishnan R <rgkrishnas@gmail.com>
2020-11-25 11:52:39 +0530

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,5 +1,6 @@
 #### This is developed by Gopal rgkrishnas@gmail.com for the main program mqtt_publish.py
 
+DEVICE_ID='0001'
 # Define the delay  time
 delay_time = 5
 # MQTT Host name or IP address with tcp
@@ -8,6 +9,6 @@ port = 1883  # MQTT port number
 user = ''  # MQTT user id if available
 password = '' # MQTT password if available
 topic = 'iot2tangle'  # MQTT message topic 
-water_measurements='litters'  # litters/gallons
+water_measurements='liters'  # liters/gallons
 water_dept_wallet_address = 'EnterAddressTowhomWalletMachineNeedsToPay' # to who the system needs to make the payment
 mywatersensor_seed='EnterYourWaterSensorwalletSeed'
